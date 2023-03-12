19d80737cacb5a67a13b88a94e4d0ccf06fe4167
itisMilan <abhaymilan8@gmail.com>
2023-03-12 11:20:08 +0530

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <h1 className='text-3xl font-bold underline'>Hello !</h1>
+    <h1 className='text-3xsdfl font-bold underline'>Hello !</h1>
   )
 }
 
